52b5e26e52cb57bb77e2e9a923435f02b59d0532
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-06-18 13:13:13 -0600

    Adds data attribute test

--- test/node_test.rb
+++ test/node_test.rb
@@ -11,4 +11,8 @@ class NodeTest < Minitest::Test
 	def test_it_exists
 		assert_instance_of Node, @node
 	end
+	
+	def test_it_accepts_initialization_argument_as_data_attribute
+		assert_equal "plop", @node.data
+	end
 end
